4c24d15144b4412245a4a9eeddc6ee2a0125d85f
Bo Ramsey <psychosledge@gmail.com>
2026-04-28 07:11:12 -0400

    fix(infrastructure): cast removeUndefinedDeep result to object before spread

--- packages/infrastructure/src/firestore-event-store.ts
+++ packages/infrastructure/src/firestore-event-store.ts
@@ -51,7 +51,7 @@ export class FirestoreEventStore implements IEventStore {
     // Remove undefined values (Firestore doesn't allow them)
     const cleanedEvent = removeUndefinedDeep(event);
 
-    await setDoc(docRef, { ...cleanedEvent, serverReceivedAt: serverTimestamp() });
+    await setDoc(docRef, { ...(cleanedEvent as object), serverReceivedAt: serverTimestamp() });
     
     // Notify subscribers after successful append
     this.notifySubscribers(event);
@@ -82,7 +82,7 @@ export class FirestoreEventStore implements IEventStore {
       for (const event of batchEvents) {
         const docRef = doc(eventsRef, event.id);
         const cleanedEvent = removeUndefinedDeep(event);
-        batch.set(docRef, { ...cleanedEvent, serverReceivedAt: serverTimestamp() });
+        batch.set(docRef, { ...(cleanedEvent as object), serverReceivedAt: serverTimestamp() });
       }
       
       // Commit batch atomically
